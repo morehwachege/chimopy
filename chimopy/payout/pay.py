5f8fa6e0f702e36bb00627c3c659a78ebe59163c
goretti16kate <gorettikate@gmail.com>
2023-02-07 18:08:06 +0300

    add airtime

--- chimopy/payout/pay.py
+++ chimopy/payout/pay.py
@@ -10,15 +10,29 @@ class Pay:
             "content-type": "application/json",
             "X-API-KEY": f"{self.api_key}"
         }
+        self.api_version = 'v0.2'
         pass
 
-    def airtime(self, country_to_send, phone_number, value_in_USD, sub_account='', turn_off_notification=False):
-        pass
+
+    def airtime(self, payment_details, sub_account='', turn_off_notification=False):
+        uri= f'{self.api_version}/payouts/airtime'
+        # note difference uri and url
+        url= f'{self.api_url}/{uri}'
+        payload = {
+            "airtimes": payment_details
+            }
+        if sub_account != '':
+            payload['subAccount'] = sub_account
+        if turn_off_notification != False:
+            payload['turnOffNotification'] = True
+        response = requests.post(url, headers=self.headers, json=payload)
+        return response.json()
+
 
     def chimoney(self, payment_details, sub_account='', turn_off_notification=False):
         ''' Payout Chimoney'''
         # payment_details == array of objects with payout information eg. email, value to send
-        uri= 'v0.2/payouts/bank'
+        uri= f'{self.api_version}/payouts/bank'
         # note difference uri and url
         url= f'{self.api_url}/{uri}'
         payload={
@@ -33,14 +47,25 @@ class Pay:
 
     
     def giftcard(self, payment_details, sub_account='', turn_off_notification=False):
-        uri = 'v0.2/payouts/gift-card'
+        uri = f'{self.api_version}/payouts/gift-card'
         url= f'{self.api_url}/{uri}'
         pass
 
     
     def bank(self, payment_details, sub_account='', turn_off_notification=False):
-        uri = 'v0.2/payouts/gift-card'
+        uri = f'{self.api_version}/payouts/bank'
         url= f'{self.api_url}/{uri}'
+        # example payment_details array
+        # payment_details = [
+        #     {
+        #             "countryToSend": "Kenya",
+        #             "account_bank": "044",
+        #             "account_number": "0690000032",
+        #             "valueInUSD": 10,
+        #             "reference": "",
+        #             "fullname" : "John Doe"
+        #     }
+        # ]
         payload = {
             "banks": payment_details
             }
@@ -49,14 +74,5 @@ class Pay:
         if turn_off_notification != False:
             payload['turnOffNotification'] = True
 
-        response = requests.post(url, headers=headers, json=payload)
-        response.json()
-        pass
-
-
-        
-# obj = Pay()
-# obj.airtime(turn_off_notification=False, country_to_send='Ghana', phone_number='254712345678', value_in_USD=3, sub_account='Antony Gakuru')
-# obj.chimoney(
-#     payment_details=pay_array_of_objects,   
-# )
+        response = requests.post(url, headers=self.headers, json=payload)
+        return response.json()
