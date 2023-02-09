b8baadd4d5257c191dc5d598afc8cafa280231e3
goretti16kate <gorettikate@gmail.com>
2023-02-09 12:20:30 +0300

    add transactions list

--- chimopy/mobile/transact.py
+++ chimopy/mobile/transact.py
@@ -2,38 +2,32 @@ import requests
 
 
 class Transaction:
-    def __init__(
-        self, 
-        base_url= 'https://chimoney-backend-api-sandbox.onrender.com/v0.2/collections/mobile-money/all',
-        api_key = None,
-        params = {} ):
-        self.base_url = base_url
-        self.params = params
+    def __init__(self, api_key, api_url):
+        self.api_key = api_key
+        self.api_url = api_url
         self.headers = {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'X-API-KEY': api_key
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": f"{self.api_key}"
         }
-        pass
+        self.api_version = 'v0.2'
 
-    def transact(self):
-        print(self.base_url)
-        res = requests.post(f'{self.base_url}/v0.2/collections/mobile-money/collect', headers=self.headers,
-        params=self.params
-        )
-        return res.json()
+    def pay(self, payment_details, sub_account=''):
+        ' Pay using mobile money'
+        uri = f'{self.api_version}/collections/mobile-money/collect'
+        url = f'{self.api_url}/{uri}'
+        payload = payment_details
+        response = requests.post(url, headers=self.headers, json=payload)
+        return response.json()
 
-    def list_transactions(self):
-        pass
-url = 'https://chimoney-backend-api-sandbox.onrender.com/v0.2/collections/mobile-money/collect'
-key = '8db691db5747c3b0f1bbbfa81d61bef2c0dc8dae7be2557fbb8db783451cddeb'
-parameters = {
-    'amount': 2,
-    'currency': 'usd',
-    'phone_number': '254740480364',
-    'fullname': 'Antony Gakuru',
-    'country': 'Kenya',
-    'tx-ref': '2123132342',
-    'subAccount': '23423efdfvd23'
-}
-send = Transaction(base_url=url ,api_key=key, params=parameters)
+    def transactions(self, sub_account='', turn_off_notification=False):
+        ''' List all mobile money transactions '''
+
+        uri = f'{self.api_version}/collections/mobile-money/all'
+        url = f'{self.api_url}/{uri}'
+        payload = {}
+        if sub_account != '':
+            payload['subAccount'] = sub_account
+
+        response = requests.post(url, headers=self.headers, json=payload)
+        return response.json()
