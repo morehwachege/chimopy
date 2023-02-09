b42842a298dbece1e46ca19a32126d19c3e26f8c
goretti16kate <gorettikate@gmail.com>
2023-02-09 13:31:51 +0300

    convert information currency

--- chimopy/info/info.py
+++ chimopy/info/info.py
@@ -1,3 +1,5 @@
+import requests
+
 class Info:
     def __init__(self, api_key, api_url):
         self.api_key = api_key
@@ -8,4 +10,35 @@ class Info:
             "X-API-KEY": f"{self.api_key}"
         }
         self.api_version = 'v0.2'
+
     
+    def countries(self):
+        '''Get a list of all supported airtime countries'''
+        
+        uri= f'{self.api_version}/info/airtime-countries'
+        url= f'{self.api_url}/{uri}'
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+
+    def assets(self, country_code=''):
+        uri= f'{self.api_version}/info/assets?countryCode={country_code}'
+        url= f'{self.api_url}/{uri}'
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    
+    def banks(self, country_code):
+        uri= f'{self.api_version}/info/country-banks?countryCode={country_code}'
+        url= f'{self.api_url}/{uri}'
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+
+    def convert(self, origin_currency, amount_in_origin_currency):
+        """ Convert currency to USD"""
+        
+        uri= f'{self.api_version}/info/local-amount-in-usd?originCurrency={origin_currency}&amountInOriginCurrency={amount_in_origin_currency}'
+        url= f'{self.api_url}/{uri}'
+        response = requests.get(url, headers=self.headers)
+        return response.json()
